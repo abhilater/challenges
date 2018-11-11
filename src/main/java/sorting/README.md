f5f2ac33da447900cc61d91b387990e842ea6948
abhishek <abhishek.gupta@helpshift.com>
2018-11-11 17:41:28 +0530

    Tried out merge sort

--- src/main/java/sorting/README.md
+++ src/main/java/sorting/README.md
@@ -24,18 +24,97 @@
 
 ```java
 static void selectionSort(Integer[] array) {
-        if (array.length < 2) return;
-
-        for (int i = 0; i < array.length - 1; i++) {
-            int min = array[i];
-            int minIdx = i;
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[j] < min) {
-                    min = array[j];
-                    minIdx = j;
-                }
+    for (int i = 0; i < array.length - 1; i++) {
+        int min = array[i];
+        int minIdx = i;
+        for (int j = i + 1; j < array.length; j++) {
+            if (array[j] < min) {
+                min = array[j];
+                minIdx = j;
             }
-            swap(array, i, minIdx);
+        }
+        swap(array, i, minIdx);
+    }
+}
+```
+
+#### Bubble sort
+![](https://cdn-images-1.medium.com/max/1600/1*gxRwophEo5u22AaSgt8JXg.jpeg)
+![](https://cdn-images-1.medium.com/max/1200/1*0G-HWCiByHVPedrKXwS8iQ.jpeg)
+
+```java
+static void bubbleSort(Integer[] array) {
+    for(int j = array.length - 1; j > 0; j--){
+        for(int i = 0 ; i < j; i++){
+            if(array[i] > array[i+1]){
+                swap(array, i, i+1);
+            }
         }
     }
+}
+```
+
+#### Insertion sort
+![](https://cdn-images-1.medium.com/max/1600/1*_W4xVv1FM3wEDZg16kvb1w.jpeg)
+![](https://cdn-images-1.medium.com/max/1200/1*NDDPPmfZx-l4dzHZw-bTmg.jpeg)
+
+```java
+static void insertionSort(Integer[] array) {
+    for (int i = 0; i < array.length; i++) {
+        int currentUnsortedItem = array[i];
+        int j = i;
+        for (; j > 0 && currentUnsortedItem < array[j - 1];
+             j--) {
+            array[j] = array[j - 1]; // shifting right
+        }
+        array[j] = currentUnsortedItem;
+    }
+}
+```
+**Note:** The best case running time of running an insertion sort algorithm on a nearly-sorted list ends up being linear — or, O(n) — since far fewer comparisons need to be made by the inner loop.
+![](https://cdn-images-1.medium.com/max/1600/1*7fLrgRmyYRkEKQ1C4YRKqw.jpeg)
+
+#### Merge sort
+![](https://cdn-images-1.medium.com/max/1600/1*T4lE_CveG7rAkEziwKtpAw.jpeg)
+
+**The basic idea behind merge sort is this: it tends to be a lot easier to sort two smaller, sorted lists rather than sorting a single large, unsorted one.**
+
+![](https://cdn-images-1.medium.com/max/1200/1*ZFpPwH6_ssRu5p8tM9T-vQ.jpeg)
+![](https://cdn-images-1.medium.com/max/1600/1*p6pvuQ0mKCYkx3ZXv6ufgw.jpeg)
+![](https://cdn-images-1.medium.com/max/1600/1*V1QaE66T3SgIIO-jv0-fHw.jpeg)
+
+```java
+static void mergeSort(Integer[] array){
+    if(array.length <= 1) return;
+
+    int n = array.length / 2;
+    List<Integer> l = Arrays.asList(array);
+    Integer[] leftArray = l.subList(0, n)
+            .toArray(new Integer[n]);
+    Integer[] rightArray = l.subList(n, l.size())
+            .toArray(new Integer[l.size() - n]);
+    mergeSort(leftArray);
+    mergeSort(rightArray);
+
+    merge(leftArray, rightArray, array);
+}
+
+static void merge(Integer[] la, 
+                  Integer[] ra, 
+                  Integer[] array) {
+    int index = 0, l = 0, r = 0;
+    while (l < la.length && r < ra.length) {
+        if (la[l] < ra[r]) {
+            array[index++] = la[l++];
+        } else {
+            array[index++] = ra[r++];
+        }
+    }
+    while (l < la.length) {
+        array[index++] = la[l++];
+    }
+    while (r < ra.length) {
+        array[index++] = ra[r++];
+    }
+}
 ```
--- src/main/java/sorting/Sorting.java
+++ src/main/java/sorting/Sorting.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.runner.RunWith;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by abhishek on 11/11/18.
@@ -14,8 +15,6 @@ import java.util.Arrays;
 public class Sorting {
 
     static void selectionSort(Integer[] array) {
-        if (array.length < 2) return;
-
         for (int i = 0; i < array.length - 1; i++) {
             int min = array[i];
             int minIdx = i;
@@ -28,6 +27,61 @@ public class Sorting {
             swap(array, i, minIdx);
         }
     }
+
+    static void bubbleSort(Integer[] array) {
+        for(int j = array.length - 1; j > 0; j--){
+            for(int i = 0 ; i < j; i++){
+                if(array[i] > array[i+1]){
+                    swap(array, i, i+1);
+                }
+            }
+        }
+    }
+
+    static void insertionSort(Integer[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int currentUnsortedItem = array[i];
+            int j = i;
+            for (; j > 0 && currentUnsortedItem < array[j - 1];
+                 j--) {
+                array[j] = array[j - 1]; // shifting right
+            }
+            array[j] = currentUnsortedItem;
+        }
+    }
+
+    static void mergeSort(Integer[] array){
+        if(array.length <= 1) return;
+
+        int n = array.length / 2;
+        List<Integer> l = Arrays.asList(array);
+        Integer[] leftArray = l.subList(0, n)
+                .toArray(new Integer[n]);
+        Integer[] rightArray = l.subList(n, l.size())
+                .toArray(new Integer[l.size() - n]);
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+
+        merge(leftArray, rightArray, array);
+    }
+
+    static void merge(Integer[] la, Integer[] ra, Integer[] array) {
+        int index = 0, l = 0, r = 0;
+        while (l < la.length && r < ra.length) {
+            if (la[l] < ra[r]) {
+                array[index++] = la[l++];
+            } else {
+                array[index++] = ra[r++];
+            }
+        }
+        while (l < la.length) {
+            array[index++] = la[l++];
+        }
+        while (r < ra.length) {
+            array[index++] = ra[r++];
+        }
+    }
+
 
     private static void swap(Integer[] array, int idx1, int idx2) {
         int t = array[idx1];
@@ -44,28 +98,45 @@ public class Sorting {
     @Property
     public void testSelectionSort(Integer[] array) {
         selectionSort(array);
-        System.out.println("Array: " + Arrays.asList(array));
+        Assert.assertTrue(isArraySorted(array, array.length));
+    }
+
+    @Property
+    public void testBubbleSort(Integer[] array) {
+        bubbleSort(array);
+        Assert.assertTrue(isArraySorted(array, array.length));
+    }
+
+    @Property
+    public void testInsertionSort(Integer[] array) {
+        insertionSort(array);
+        Assert.assertTrue(isArraySorted(array, array.length));
+    }
+
+    @Property
+    public void testMergeSort(Integer[] array) {
+        mergeSort(array);
         Assert.assertTrue(isArraySorted(array, array.length));
     }
 
     public static void main(String[] args) {
-//        Integer[] a = new Integer[]{};
-//        Integer[] b = {1};
-//        Integer[] c = {1, 2, 3};
-//        Integer[] d = {3, 2, 1};
-//        Integer[] e = {5, 3, 6, 5, 0, -8, 99, 1};
-//        Integer[] f = {0, 5, 0, -5};
-//        selectionSort(a);
-//        selectionSort(b);
-//        selectionSort(c);
-//        selectionSort(d);
-//        selectionSort(e);
-//        selectionSort(f);
-//        System.out.println(Arrays.asList(a));
-//        System.out.println(Arrays.asList(b));
-//        System.out.println(Arrays.asList(c));
-//        System.out.println(Arrays.asList(d));
-//        System.out.println(Arrays.asList(e));
-//        System.out.println(Arrays.asList(f));
+        Integer[] a = new Integer[]{};
+        Integer[] b = {1};
+        Integer[] c = {1, 2, 3};
+        Integer[] d = {3, 2, 1};
+        Integer[] e = {5, 3, 6, 5, 0, -8, 99, 1};
+        Integer[] f = {0, 5, 0, -5};
+        //mergeSort(a);
+        //mergeSort(b);
+        mergeSort(c);
+        //mergeSort(d);
+        //mergeSort(e);
+        //mergeSort(f);
+        //System.out.println(Arrays.asList(a));
+        //System.out.println(Arrays.asList(b));
+        System.out.println(Arrays.asList(c));
+        //System.out.println(Arrays.asList(d));
+        //System.out.println(Arrays.asList(e));
+        //System.out.println(Arrays.asList(f));
     }
 }
